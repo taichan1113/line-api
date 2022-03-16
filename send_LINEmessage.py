2290ded3876b7a6a947c4d4e86be2c139c4d812e
Taichi Takemoto <takecchi.1113@gmail.com>
2022-03-16 21:48:10 +0900

    change directory

--- send_LINEmessage.py
+++
@@ -1,7 +0,0 @@
-import requests
-url = "https://notify-api.line.me/api/notify" 
-token = "tYjavU1eyvPoqVh66WTFIIiFZ2Fo83NhzNq6rX7JQ7o"
-headers = {"Authorization" : "Bearer "+ token} 
-message =  "サンシュユからメッセージ" 
-payload = {"message" :  message} 
-r = requests.post(url, headers = headers, params=payload) 
