3130a5908fddff599ed29fbc626df596512ac740
Taichi Takemoto <takecchi.1113@gmail.com>
2022-03-16 22:14:18 +0900

    make it better

--- test_repeatBot.py
+++ test_repeatBot.py
@@ -48,5 +48,6 @@ def handle_follow(event):
 
 
 if __name__ == "__main__":
-   port = int(os.getenv("PORT"))
-   app.run(host="0.0.0.0", port=port)
+#    port = int(os.getenv("PORT"))
+#    app.run(host="0.0.0.0", port=port)
+    app.run()
