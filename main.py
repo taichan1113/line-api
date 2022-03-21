f4e5b685ba404aaa2b16cc636845a3a61ad67930
Taichi Takemoto <takecchi.1113@gmail.com>
2022-03-21 21:22:04 +0900

    led_on message

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+import paho.mqtt.publish as publish
 import os
 from linebot import (
    LineBotApi, WebhookHandler
@@ -12,18 +13,28 @@ from linebot.models import (
 )
 
 app = Flask(__name__)
- #環境変数取得 
+# 環境変数取得 
 MY_CHANNEL_ACCESS_TOKEN = os.environ["MY_CHANNEL_ACCESS_TOKEN"]
 MY_CHANNEL_SECRET = os.environ["MY_CHANNEL_SECRET"]
 MY_BEEBOTTE_TOKEN = os.environ["MY_BEEBOTTE_TOKEN"]
- #APIの設定 
+# APIの設定 
 line_bot_api = LineBotApi(MY_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(MY_CHANNEL_SECRET)
- #テスト用 
+
+# MQTT publish
+def publish_gpio_control_msg(msg):
+    publish.single('Documents/watering_system', \
+                    msg, \
+                    hostname='mqtt.beebotte.com', \
+                    port=8883, \
+                    auth = {'username':'token:{}'.format(MY_BEEBOTTE_TOKEN)}, \
+                    tls={'ca_certs':'mqtt.beebotte.com.pem'})
+
+# get test
 @app.route("/")
 def hello_world():
    return "hello world!"
- #Webhookからのリクエストをチェック 
+# Webhook request
 @app.route("/callback", methods=['POST'])
 def callback():
    signature = request.headers['X-Line-Signature']
@@ -34,13 +45,19 @@ def callback():
    except InvalidSignatureError:
        abort(400)
    return 'OK'
- #オウム返しプログラム 
+
+# オウム返しプログラム 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-   line_bot_api.reply_message(
-       event.reply_token,
-       TextSendMessage(text=event.message.text))
- #友達追加時イベント 
+    msg = event.message.text.encode('utf-8')
+    led_msg = [s.encode('utf-8') for s in ['LED', '電気']]
+    if msg in led_msg:
+        publish_gpio_control_msg('on')
+    else:
+        line_bot_api.reply_message(
+           event.reply_token,
+           TextSendMessage(text=event.message.text))
+# 友達追加時イベント 
 @handler.add(FollowEvent)
 def handle_follow(event):
    line_bot_api.reply_message(
