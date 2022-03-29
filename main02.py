2a7a9d0d20c62b1cadc8841de75c9ccf0babc60d
Taichi Takemoto <takecchi.1113@gmail.com>
2022-03-29 22:24:03 +0900

    print get_msg

--- main02.py
+++ main02.py
@@ -70,6 +70,7 @@ def on_connect(client, userdata, rc):
 
 def on_message(client, userdata, msg):
     get_msg = msg.payload.decode('utf-8')
+    print(get_msg)
     line_bot_api.push_message(MY_LINE_USER_ID, TextSendMessage(text=get_msg))
     
 if __name__ == "__main__":
