b8856a1ceeb666a13cdd6fd7ab1ee9f10505ca0f
taichan <takecchi.1113@gmail.com>
2022-05-09 06:21:42 +0900

    service package is under development

---
+++ Service/Services.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+
+class Services:
+  def __init__(self, beebotteToken, mqttTopic):
+    self.mqttTopic = mqttTopic
+    self.beebotteToken = beebotteToken
+
+    client = mqtt.Client()
+    client.tls_set("mqtt.beebotte.com.pem")
+    client.username_pw_set("token:{}".format(self.beebotteToken))
+    client.connect("mqtt.beebotte.com", 8883, 60)
+
+    client.on_connect = self.onConnect
+    client.on_message = self.onMessage
+    client.loop_start()
+
+  # MQTT methods
+  def onConnect(self, client, userdata, flag, rc):
+    client.subscribe(self.mqttTopic, 1)
+
+  def onMessage(self, client, userdata, msg):
+    message = msg.payload.decode('utf-8')
+    # line_bot_api.push_message(MY_LINE_USER_ID, TextSendMessage(text=message))
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask, request, abort
-
-import paho.mqtt.client as mqtt
-
 import os
+
+from flask import Flask, request, abort
 
 from linebot import (
    LineBotApi, WebhookHandler
@@ -15,27 +13,26 @@ from linebot.models import (
    ImageMessage, AudioMessage, PostbackEvent
 )
 
+from Service import Services
+
 app = Flask(__name__)
-# 環境変数取得 
+# ENV. variable 
 MY_CHANNEL_ACCESS_TOKEN = os.environ["MY_CHANNEL_ACCESS_TOKEN"]
 MY_CHANNEL_SECRET = os.environ["MY_CHANNEL_SECRET"]
 MY_LINE_USER_ID = os.environ["MY_LINE_USER_ID"]
 MY_BEEBOTTE_TOKEN = os.environ["MY_BEEBOTTE_TOKEN"]
-# APIの設定 
+# LINE API settings
 line_bot_api = LineBotApi(MY_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(MY_CHANNEL_SECRET)
-# MQTT
+# MQTT settings
 MQTT_TOPIC = 'home_IoT/watering_system'
-client = mqtt.Client()
-client.tls_set("mqtt.beebotte.com.pem")
-client.username_pw_set("token:{}".format(MY_BEEBOTTE_TOKEN))
-client.connect("mqtt.beebotte.com", 8883, 60)
 
-# get test
+# Test (GET)
 @app.route("/")
 def hello_world():
   return "hello world!"
-# Webhook request
+
+# Webhook request (POST)
 @app.route("/callback", methods=['POST'])
 def callback():
   signature = request.headers['X-Line-Signature']
@@ -47,46 +44,32 @@ def callback():
     abort(400)
   return 'OK'
 
-# ポストバック
+# postback
 @handler.add(PostbackEvent)
 def handle_postback(event):
+  service = Services.Services(MY_BEEBOTTE_TOKEN)
+
   userID = event.source.user_id
   data = event.postback.data
   line_bot_api.push_message(userID, TextSendMessage(text=data))
+  
+  # client.publish(MQTT_TOPIC, data, 1)
 
-# オウム返し
+# repeat message bot
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
   msg = event.message.text.encode('utf-8')
-  userID = event.source.user_id
-  test_msg = [s.encode('utf-8') for s in ['test', 'テスト']]
-  if msg in test_msg:
-    line_bot_api.push_message(userID, TextSendMessage(text='publishing...'))
-    client.publish(MQTT_TOPIC, userID, 1)
-  else:
-    line_bot_api.reply_message(
-      event.reply_token,
-      TextSendMessage(text=event.message.text))
+  line_bot_api.reply_message(
+    event.reply_token,
+    TextSendMessage(text=msg))
 
-# 友達追加時
+# follow event
 @handler.add(FollowEvent)
 def handle_follow(event):
   line_bot_api.reply_message(
     event.reply_token,
     TextSendMessage(text='友達追加ありがとう'))
 
-def on_connect(client, userdata, flag, rc):
-  client.subscribe(MQTT_TOPIC, 1)
-
-def on_message(client, userdata, msg):
-  userID = msg.payload.decode('utf-8')
-  line_bot_api.push_message(userID, TextSendMessage(text='test message published correctly'))
-    
 if __name__ == "__main__":
-    
-  client.on_connect = on_connect
-  client.on_message = on_message
-  client.loop_start()
-  
   port = int(os.getenv("PORT"))
   app.run(host="0.0.0.0", port=port)
